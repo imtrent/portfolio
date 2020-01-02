3d8aa7e31083c536e1efb95288947127cd48d7be
Ian Trent <ian.m.trent@gmail.com>
2020-01-01 22:38:09 -0500

    Added content to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,21 @@
-import React from "react"
+import React from "react";
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <SEO title="404: Not found" />
+        <h1>404</h1>
+        <p>Oh no! It looks like the webpage you’re trying to reach does not exist.</p>
+        <Link
+            to="/"
+            exact
+        >
+            Back To Home
+        </Link>
+    </Layout>
 )
 
 export default NotFoundPage
