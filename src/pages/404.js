3ce1b96f1c2050fe1f41d86aa7cec2feec04115a
Ian Trent <ian.m.trent@gmail.com>
2020-01-11 11:42:59 -0500

    Styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,16 +5,18 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
-    <Layout>
-        <SEO title="404: Not found" />
-        <h1>404</h1>
-        <p>
-            Oh no! It looks like the webpage you’re trying to reach does not
-            exist.
-        </p>
-        <Link to="/" exact>
-            Back To Home
-        </Link>
+    <Layout className="not-found">
+        <SEO title="404 Not found" />
+        <div className="grid-container text-center">
+            <h1>404</h1>
+            <p>
+                Oh no! It looks like the webpage you’re trying to reach does not
+                exist.
+            </p>
+            <Link to="/" exact className="link">
+                Back To Home
+            </Link>
+        </div>
     </Layout>
 );
 
