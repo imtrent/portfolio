c0f2cb48fd16137785c2f6d22d25530fc81fa535
Ian Trent <ian.m.trent@gmail.com>
2020-05-30 18:16:06 -0400

    Made form inputs required

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,26 +17,26 @@ const Contact = () => {
               <label htmlFor="name" className="block mb-1 leading-5 text-mdgray">
                 Name
               </label>
-              <input type="text" id="name" name="name" className="mb-6 sm:mb-0 appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none" />
+              <input type="text" id="name" name="name" className="mb-6 sm:mb-0 appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none" required />
             </div>
             <div className="w-full sm:w-1/2 sm:ml-4">
               <label htmlFor="email" className="block mb-1 leading-5 text-mdgray">
                 Email
               </label>
-              <input type="email" id="email" name="email" className="appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none" />
+              <input type="email" id="email" name="email" className="appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none" required />
             </div>
           </div>
           <div className="mb-6">
             <label htmlFor="subject" className="block mb-1 leading-5 text-mdgray">
               Subject
             </label>
-            <input type="text" id="subject" name="subject" className="appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none" />
+            <input type="text" id="subject" name="subject" className="appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none" required/>
           </div>
           <div>
             <label htmlFor="message" className="block mb-1 leading-5 text-mdgray">
               Message
             </label>
-            <textarea type="text" id="message" name="message" className="h-40 resize-none appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none"></textarea>
+            <textarea type="text" id="message" name="message" className="h-40 resize-none appearance-none border rounded w-full py-2 px-2 text-mdgray leading-tight focus:border-ltgray focus:outline-none" required></textarea>
           </div>
           <button className="mt-5 bg-primary text-white px-6 py-2 rounded">Submit</button>
         </form>
